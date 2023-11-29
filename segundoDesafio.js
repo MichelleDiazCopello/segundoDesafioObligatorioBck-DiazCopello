6ce29137c1b75f8e7d3b1a6a09692723bf5fc5c4
Michelle Diaz Copello <m.diazcopello@gmail.com>
2023-11-29 19:17:24 -0300

    news

--- segundoDesafio.js
+++ segundoDesafio.js
@@ -9,16 +9,7 @@ class ProductManager {
 
     static id = 0;
 
-    async addProduct ( /*title, description, price, thumbnail, id, stock*/ product ) {
-        // ProductManager.id++;
-        // const product = {
-        //     id: ProductManager.id,
-        //     title,
-        //     description,
-        //     price,
-        //     thumbnail,
-        //     stock,
-        // };
+    async addProduct ( product ) {
         const products = await this.getProducts ();
         products.push ( product );
         await fs.promises.writeFile ( this.path, JSON.stringify ( products ), 'utf-8' );
@@ -35,53 +26,39 @@ class ProductManager {
         };
     };
 
-
-
-//     getProductsByCode ( productCode ) {
-//         const product = this.getProduct ( productCode );
-//         if ( product ) {
-//             if ( !product.code.includes ( productCode ) ) {
-//                 product.code.push (  );
-//             }
-//         }
-//         else {
-//             console.log ( "Not Found" );
-//         }
-//     }
-}
-
-const db = async () => {
-    const productManager = new ProductManager ( './products.json' ); 
-    let data = await productManager.getProducts ();
-
-    const almonds = {
-        title: "Almond",
-        description: "Almonds are a type of tree nut with many health benefits. They are an excellent source of vitamin E, magnesium, and fiber. Almonds are also high in protein and healthy fats, making them a great snack option.",
-        price: 5.99,
-        thumbnail: "https://acdn.mitiendanube.com/stores/960/315/products/almendras-enteras-non-pareil-peladas-chile-1kg-201606300743071-383879671f6a353e6415525085451904-640-0.jpg", 
-        stock: 100
-    };
-    const cashews = {
-        title: "Cashew",
-        description: "Cashews are a type of tree nut that are rich in nutrients. They are a good source of protein, healthy fats, and minerals like copper, magnesium, and zinc. Cashews are also a good source of antioxidants, which can help protect against disease.",
-        price: 6.99,
-        thumbnail: "https://www.activearthfood.com.au/cdn/shop/products/cashew-nuts.jpg?v=1593169570", 
-        stock: 100
+    async modifyProduct () {
+        const modify = await fs.promises.appendFile ( this.path, 'utf-8');
+        return modify;
     };
 
-    await productManager.addProduct ( almonds );
-    await productManager.addProduct ( cashews );
-    data = await productManager.getProducts ();
-    console.log ( data );
+    async deleteProduct () {
+        const deleteProd = await fs.promises.unlink ( this.path );
+        return deleteProd;
+    };
 };
+    const db = async () => {
+        const productManager = new ProductManager ( './products.json' ); 
+        let data = await productManager.getProducts ();
+
+        const almonds = {
+            title: "Almond",
+            description: "Almonds are a type of tree nut with many health benefits. They are an excellent source of vitamin E, magnesium, and fiber. Almonds are also high in protein and healthy fats, making them a great snack option.",
+            price: 5.99,
+            thumbnail: "https://acdn.mitiendanube.com/stores/960/315/products/almendras-enteras-non-pareil-peladas-chile-1kg-201606300743071-383879671f6a353e6415525085451904-640-0.jpg", 
+            stock: 100
+        };
+        const cashews = {
+            title: "Cashew",
+            description: "Cashews are a type of tree nut that are rich in nutrients. They are a good source of protein, healthy fats, and minerals like copper, magnesium, and zinc. Cashews are also a good source of antioxidants, which can help protect against disease.",
+            price: 6.99,
+            thumbnail: "https://www.activearthfood.com.au/cdn/shop/products/cashew-nuts.jpg?v=1593169570", 
+            stock: 100
+        };
+
+        await productManager.addProduct ( almonds );
+        await productManager.addProduct ( cashews );
+        data = await productManager.getProducts ();
+        console.log ( data );
+    };
 
 db ();
-
-// const product = new ProductManager ();
-// product.addProduct ('Almond', 'Almonds are a type of tree nut with many health benefits. They are an excellent source of vitamin E, magnesium, and fiber. Almonds are also high in protein and healthy fats, making them a great snack option.', 5.99, 'https://acdn.mitiendanube.com/stores/960/315/products/almendras-enteras-non-pareil-peladas-chile-1kg-201606300743071-383879671f6a353e6415525085451904-640-0.jpg', 1, 100 );
-
-// product.addProduct ('Cashews', 'Cashews are a type of tree nut that are rich in nutrients. They are a good source of protein, healthy fats, and minerals like copper, magnesium, and zinc. Cashews are also a good source of antioxidants, which can help protect against disease.', 5.99, 'https://www.activearthfood.com.au/cdn/shop/products/cashew-nuts.jpg?v=1593169570', 2, 100 );
-
-
-// const products = product.getProducts ();
-// console.log ( products );
